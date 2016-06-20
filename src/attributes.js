fb2d2a4bef8a00ec82603baf937e883ea8210213
acstll <19@8302.net>
2016-06-20 16:01:27 +0200

    Add attributes helper factory

---
+++ src/attributes.js
@@ -0,0 +1,58 @@
+
+module.exports = function createAttributes () {
+  var _attributes = []
+
+  return function attributes () {
+    var key = arguments[0]
+    var value = arguments[1]
+
+    if (arguments.length === 1) {
+      return get(key)
+    }
+    if (arguments.length === 2) {
+      return set(key, value)
+    }
+
+    return _attributes
+  }
+
+  function get (key) {
+    var index = findIndex(_attributes, key)
+    return _attributes[index] ? _attributes[index].value : null
+  }
+
+  function set (key, value) {
+    var index = findIndex(_attributes, key)
+    var item = {
+      key: key,
+      value: value
+    }
+
+    if (index > -1) {
+      if (value === null || typeof value === 'undefined') {
+        _attributes.splice(index, 1)
+      } else {
+        _attributes.splice(index, 1, item)
+      }
+      return
+    }
+    if (typeof value === 'object' || typeof value === 'function') {
+      throw new TypeError(
+        'Only strings, numbers or arrays are allowed. ' +
+        'Tried to set `' + key + '` to null, object or function.'
+      )
+    }
+    _attributes.push(item)
+  }
+}
+
+function findIndex (target, key) {
+  var index = -1
+  target.some(function (value, i) {
+    if (value.key === key) {
+      index = i
+      return true
+    }
+  })
+  return index
+}
