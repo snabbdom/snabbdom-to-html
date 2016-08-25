a6fc5ae237e4cb4ad46f1ebbba08a510c2105c13
acstll <19@8302.net>
2016-08-25 21:00:08 +0200

    Add classList behaviour to class module. Fixes #20

--- modules/class.js
+++ modules/class.js
@@ -1,16 +1,28 @@
 
 var forOwn = require('lodash.forown')
+var remove = require('lodash.remove')
+var uniq = require('lodash.uniq')
 
 // data.class
 
 module.exports = function classModule (vnode, attributes) {
-  var values = []
+  var values
+  var _add = []
+  var _remove = []
   var classes = vnode.data.class || {}
+  var existing = attributes.get('class')
+  existing = existing.length > 0 ? existing.split(' ') : []
 
   forOwn(classes, function (value, key) {
     if (value === true) {
-      values.push(key)
+      _add.push(key)
+    } else {
+      _remove.push(key)
     }
+  })
+
+  values = remove(uniq(existing.concat(_add)), function (value) {
+    return _remove.indexOf(value) < 0
   })
 
   if (values.length) {
